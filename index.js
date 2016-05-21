6d22bad4920c1684fa2a4286a0caba2f8e9e5bf2
Daniel Huan Nguyen <danielhuannguyen@gmail.com>
2016-05-20 20:36:02 -0500

    added some example code

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // TODO: Use callbacks in getting a webpage.
+var https = require('https');
+
+var google = "www.google.com";
+var epic = "www.epic.com";
+var yahoo = "www.yahoo.com";
+var duckduckgo = "duckduckgo.com"
+
+var printSite = function (hostname, callback) {
+	var site = "";
+	var options = {
+		// protocol: "https:",
+		'hostname': hostname
+	};
+	var req = https.request(options, (res) => {
+		res.on('data', (chunk) => {
+			site += chunk;
+		}).on ('end', () => {
+			callback(site);
+		});
+	}).on ('error', (err) => {
+		console.log(err);
+	});
+
+	req.end();
+};
+
+printSite(duckduckgo, (site) => console.log(site));
